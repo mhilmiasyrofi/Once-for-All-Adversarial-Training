64c46003ccb7678b12d9313727b90492acd83c8f
Muhammad Hilmi ASYROFI <mhilmiasyrofi@gmail.com>
2020-12-10 09:19:01 +0800

    add script for adversarial training

---
+++ train.sh
@@ -0,0 +1,31 @@
+
+#!/bin/bash
+ 
+# Declare an array of string with type
+# declare -a adv=("autoattack" "autopgd" "bim" "cw" "deepfool" "newtonfool" "fgsm" "newtonfool" "pgd" "pixelattack" "spatialtransformation" "squareattack")
+
+# declare -a adv=("autoattack" "autopgd" "bim" "cw" "deepfool")
+
+# declare -a adv=("fgsm" "newtonfool" "pgd")
+
+# declare -a adv=("pixelattack" "spatialtransformation" "squareattack")
+
+declare -a adv=("autopgd" "pixelattack")
+
+# Iterate the string array using for loop
+for a in ${adv[@]}; do
+    python OAT.py \
+        --adversarial_data $a \
+        --dataset cifar10 \
+        --batch_size 100 \
+        --epochs 30 \
+        --use2BN
+done
+
+
+# python OAT.py \
+#     --adversarial_data deepfool \
+#     --dataset cifar10 \
+#     --batch_size 100 \
+#     --epochs 30 \
+#     --use2BN
